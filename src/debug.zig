93bcf4593410ce84cd8973631c299b5589901c50
last_arg <last_arg@tutanota.com>
2021-01-09 19:26:42 +0200

    little function to make notcurses debugging easier

---
+++ src/debug.zig
@@ -0,0 +1,7 @@
+const std = @import("std");
+
+pub fn ttyWarn(comptime format: []const u8, args: anytype) void {
+    const file = std.fs.cwd().openFile("/dev/pts/9", .{ .write = true }) catch unreachable;
+    defer file.close();
+    file.writer().print(format, args) catch unreachable;
+}
